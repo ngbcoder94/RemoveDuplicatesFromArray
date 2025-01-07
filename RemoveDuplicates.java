60924bb362e14989cb4f25090ce97b8ed56fbb05
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-06 22:48:24 -0500

    removeDuplicates() function added

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -7,7 +7,16 @@
  * Status: WIP
  */
 public class RemoveDuplicates{
+        
+        /*
+         * Here is my solution function that will remove all duplicates in an array.
+         *
+         * @param int[] nums - Array passed in.
+         * @return int - Return the new length of the array after I remove the duplicates.
+         */
+        public int removeDuplicates(int[] nums){
 
+        }
         public static void main(String[] args){
                 System.out.println("Hello World");
         }
