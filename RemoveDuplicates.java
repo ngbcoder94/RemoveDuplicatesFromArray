34f8f67e0460d06e25147b85035d7348ac0f4d08
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-06 21:54:47 -0500

    RemoveDuplicates class created for solution

---
+++ RemoveDuplicates.java
@@ -0,0 +1,14 @@
+/*
+ * Here is my class that I will implement in order to solve the RemoveDuplicates problem.
+ *
+ * Author: Nick Burkett.
+ * Date: 1/6/2025
+ *
+ * Status: WIP
+ */
+public class RemoveDuplicates{
+
+        public static void main(String[] args){
+                System.out.println("Hello World");
+        }
+}
